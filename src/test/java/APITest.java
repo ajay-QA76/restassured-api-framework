19fdc28835d5b542befb052dc9c6766c8dabc7b6
ajay-QA76 <ajay.testing.work@gmail.com>
2026-05-24 17:40:00 +0530

    Update APITest.java

--- src/test/java/APITest.java
+++ src/test/java/APITest.java
@@ -1,1 +1,17 @@
+package com.api.tests;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class APITest {
+
+    @Test
+    public void getUserTest() {
+
+        int statusCode = 200;
+
+        Assert.assertEquals(statusCode, 200);
+
+        System.out.println("API Test Passed");
+    }
+}
